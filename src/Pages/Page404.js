55dbb3d78156100e8b575a73defa8503b5abcedb
Dawid Cybert <dawidcybert@gmail.com>
2022-11-26 14:36:00 +0100

    Dodanie strony wypozyczalni i 404

---
+++ src/Pages/Page404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page404 = () => {
+    return (
+        <><h1>Błąd 404</h1>
+            <p>Nieprawidłowy adres strony...</p>
+        </>
+    )
+}
+
+export default Page404
---
+++ src/Pages/Rental.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Rental = () => {
+    return (
+        <><h1>Wypożyczalnia</h1>
+            <p>Jeszcze do końca nie wiadomo jak będzie wyglądała...</p>
+        </>
+    )
+}
+
+export default Rental
