616bd1660ea1fd67cc1e8ac0ee0d953a3b7e2019
Dawid Cybert <dawidcybert@gmail.com>
2023-01-07 18:56:04 +0100

    Pierwszy szlif footera

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,6 +3,7 @@ import { Grid, Typography } from '@mui/material'
 import { styled, useTheme } from '@mui/material/styles';
 import { Link } from 'react-router-dom';
 
+import Logo from '../images/bumerang_logo_white.png'
 import './Footer.scss'
 
 import prow from '../images/prow.jpg'
@@ -11,18 +12,42 @@ const Footer = () => {
     return (
         <div id="footer">
             <Grid container justify="center" alignItems="center" spacing={2}>
-                <Grid item xs={6} alignContent="center" alignItems="center" >
-                    <h1 style={{ textAlign: 'center' }}>Wypożyczalnia Bumerang</h1>
 
+                <Grid container item className='footer__center' sm={12} md={4} alignContent="center" alignItems="center" direction="column">
+                    <Grid item>
+                        <a href="geo:37.786971,-122.399677;u=35"><Typography className='address' style={{ textAlign: 'center' }}>Mauryca 27 k. Łasku</Typography></a>
+                    </Grid>
+
+                    <Grid item>
+                        <a className='tel' itemProp="telephone" href="tel:694 847 867">694 847 867</a>
+                    </Grid>
+
+                    <Grid item>
+                        <a className='tel' itemProp="telephone" href="tel:669 336 693">669 336 693</a>
+                    </Grid>
                 </Grid>
-                <Grid item xs={6} alignContent="center" alignItems="center">
+
+
+                <Grid className='footer__left' container item sm={6} md={4} alignContent="center" direction="column">
+                    <img className="logo" src={Logo} alt="Strona startowa" />
+                    <Link to={`/regulamin`}
+                        style={{ marginTop: '1rem' }}>
+                        <Typography variant="body1">Regulamin wypożyczalni</Typography>
+                    </Link>
+                    <Link to={`/rodo`}
+                        style={{ marginTop: '1rem' }}>
+                        <Typography variant="body1">RODO</Typography>
+                    </Link>
+                </Grid>
+
+
+
+
+                <Grid className='footer__right' container item sm={6} md={4} alignContent="center" alignItems="center" direction="column">
                     <Link to={`/prow`}
                         style={{ marginTop: '1rem' }}>
                         <img src={prow} alt="Program Rozwoju Obszarów Wiejskich na lata 2014-2020" />
                     </Link>
-                    <Typography variant="body1">Regulamin wypożyczalni</Typography>
-                    <Typography variant="body1">RODO</Typography>
-                    <Typography variant="body1">Co gdzie i jak</Typography>
 
                 </Grid>
             </Grid>
